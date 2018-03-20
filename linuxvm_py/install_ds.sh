f8c8a4ca209427b49b4221e334de5d57b4d6f390
Joben Rara <joben.rara@gmail.com>
2018-03-20 11:52:24 +0800

    Update install_ds.sh

--- linuxvm_py/install_ds.sh
+++ linuxvm_py/install_ds.sh
@@ -5,19 +5,29 @@ sudo apt-get update
 #install xrdp
 #reference: https://buildazure.com/2018/02/28/how-to-setup-an-ubuntu-linux-vm-in-azure-with-remote-desktop-rdp-access/
 
+# install remote desktop
 sudo apt-get install lxde -y
 sudo apt-get install xrdp -y
 sudo /etc/init.d/xrdp start
 
-sudo wget https://repo.continuum.io/archive/Anaconda3-4.3.1-Linux-x86_64.sh -O ~/anaconda.sh
-bash ~/anaconda.sh -b -p $HOME/anaconda 
-rm ~/anaconda.sh 
-export PATH="$HOME/anaconda/bin:$PATH"
+# install vscode
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo apt-get update
+sudo apt-get install code # or code-insiders
 
-echo 'export PATH="$HOME/anaconda/bin:$PATH"' >> ~/.bashrc 
+# install anaconda
+sudo wget -C https://repo.continuum.io/archive/Anaconda3-4.3.1-Linux-x86_64.sh
+
+#bash ~/anaconda.sh -b -p $HOME/anaconda 
+#rm ~/anaconda.sh 
+#export PATH="$HOME/anaconda/bin:$PATH"
+
+#echo 'export PATH="$HOME/anaconda/bin:$PATH"' >> ~/.bashrc 
 
 # Refresh basically
-source ~/.bashrc
+#source ~/.bashrc
 
 #conda update conda
 
